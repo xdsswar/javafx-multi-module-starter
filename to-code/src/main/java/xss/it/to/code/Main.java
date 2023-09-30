508fab62ca32680b4331f23dcdd9cd20abfaa43b
xdsswar <xdsswar@gmail.com>
2023-09-30 16:19:40 -0400

    Initial commit

---
+++ to-code/src/main/java/xss/it/to/code/Main.java
@@ -0,0 +1,20 @@
+package xss.it.to.code;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author XDSSWAR
+ * Created on 09/30/2023
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
